0b42ae5edcfa4ac493af835207cd09fcdc670ac5
Joel Eriba <joel.eriba@decagon.dev>
2024-05-29 18:21:25 +0100

    Add files via upload

---
+++ ArrayTotal.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CT_Assessment1
+{
+    public class ArrayTotal
+    {
+        public static int SumArray(int[] numbers)
+        {
+            int total = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    total += 1;
+                }
+                else
+                {
+                    total += 3;
+                }
+                if (numbers[i] == 8)
+                {
+                    total += 5;
+                }
+            }
+            return total;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using CT_Assessment1;
+public class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = {15,25,35};
+        int total = ArrayTotal.SumArray(numbers);
+        Console.WriteLine("Total Score: " + total);
+    }
+}
